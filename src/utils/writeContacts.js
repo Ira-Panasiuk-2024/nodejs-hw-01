03908143cd19d4999ae9e7a1fad72917e02bb91d
Ira Panasiuk <panasykira2022@gmail.com>
2025-02-20 13:25:13 +0200

    update files

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,10 @@
+import fs from 'fs';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+    try {
+        await fs.promises.writeFile(PATH_DB, JSON.stringify(updatedContacts));
+    } catch (error) {
+        console.error(error);
+    }
+};
