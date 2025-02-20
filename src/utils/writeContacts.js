33e7f59d219329ea7374912d69eed03b8ca31bd3
Ira Panasiuk <panasykira2022@gmail.com>
2025-02-20 20:04:20 +0200

    update files

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -3,7 +3,7 @@ import { PATH_DB } from '../constants/contacts.js';
 
 export const writeContacts = async (updatedContacts) => {
     try {
-        await fs.promises.writeFile(PATH_DB, JSON.stringify(updatedContacts));
+        await fs.promises.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
     } catch (error) {
         console.error(error);
     }
